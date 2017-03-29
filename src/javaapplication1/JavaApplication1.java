e4bdaf3fa4da6974f6749a890f72158db36da0fe
Davi Rangel <davirangel95@hotmail.com>
2017-03-29 00:03:12 -0300

    primeiro commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 
-/**
- *
- * @author bichaomemo
- */
 public class JavaApplication1 {
 
-    printf("alo");
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Teste de aplicação!");
     }
     
 }

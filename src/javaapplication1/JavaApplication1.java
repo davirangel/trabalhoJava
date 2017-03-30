43de20b17c88da218dc25e042d5b176d28d93af8
Luis Felipe Macena <luismacena.adr@gmail.com>
2017-03-29 23:35:46 -0300

    adc class Motor

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -3,10 +3,8 @@ package javaapplication1;
 
 public class JavaApplication1 {
 
-   
     public static void main(String[] args) {
         System.out.println("Teste de aplicação!");
         System.out.println("Segundo teste de aplicação!");
     }
-    
 }

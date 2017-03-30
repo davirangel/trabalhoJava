5ce2343a1f60f08b5e090bd23983604adb5fb12d
Luis Felipe Macena <luismacena.adr@gmail.com>
2017-03-30 16:10:53 -0300

    construtores em lancha

--- src/javaapplication1/Lancha.java
+++ src/javaapplication1/Lancha.java
@@ -3,10 +3,48 @@ public class Lancha extends VeiculoMaritimo {
     String tipoCasco;
     boolean banheiro;
     
+    /* CONSTRUTORES */
+    public Lancha (String marca, String modelo, String id, int numeroPassageiros, int velocidadeMaxima, float preco){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.id = id;
+        this.numeroPassageiros = numeroPassageiros;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.preco = preco;
+    }
+    public Lancha(String marca, String modelo, String id) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.id = id;
+    }
+    public Lancha(String modelo, boolean banheiro, float preco){
+        this.modelo = modelo;
+        this.banheiro = banheiro;
+        this.preco = preco;
+    }
+    public Lancha(int numeroPassageiros, int velocidadeMaxima){
+        this.numeroPassageiros = numeroPassageiros;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+    public Lancha(String tipoCasco, boolean banheiro){
+        this.tipoCasco = tipoCasco;
+        this.banheiro = banheiro;
+    }
+    public Lancha(String tipoCasco){
+        this.tipoCasco = tipoCasco;
+    }
+    public Lancha(boolean banheiro) {
+        this.banheiro = banheiro;
+    }
+    public Lancha(float preco) {
+        this.preco = preco;
+    }
     public Lancha() {
         
     }
     
+    
+    /* GETTERS E SETTERS */
     public String getTipoCasco() {
         return tipoCasco;
     }
@@ -23,6 +61,7 @@ public class Lancha extends VeiculoMaritimo {
         this.banheiro = banheiro;
     }
     
+    /* METODOS */
     public void imprimir() {
         System.out.println("TIPO DO CASCO: " + getTipoCasco());
         System.out.println("BANHEIRO: " + isBanheiro());
